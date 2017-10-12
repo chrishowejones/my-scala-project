b70f134d1830ba969f735864e3db1da56a29298f
Chris Howe-Jones <chris.howejones@gmail.com>
2017-10-12 17:29:33 +0100

    Added factorial and fibonacci

--- src/main/scala/example/MyModule.scala
+++ src/main/scala/example/MyModule.scala
@@ -4,16 +4,42 @@ package example
 /* Another comment */
 /** A documentation comment */
 object MyModule {
+
   def abs(n: Int): Int =
     if (n < 0) -n
     else n
+
+  def factorial(n: Int): Int = {
+    def go(n: Int, acc: Int): Int = {
+      if (n <= 0) acc
+      else go(n-1, n*acc)
+    }
+
+    go(n, 1)
+  }
 
   private def formatAbs(x: Int) = {
     val msg = "The absolute value of %d is %d"
     msg.format(x, abs(x))
   }
 
-  def main(args: Array[String]): Unit =
-    println(formatAbs(-111))
+  private def formatFactorial(n:Int) = {
+    val msg = "The factorial of %d is %d."
+    msg.format(n, factorial(n))
+  }
+
+  def fibonacci(n: Int): Int = {
+    @annotation.tailrec
+    def go(n:Int, a: Int, b:Int): Int =
+      if (n <= 1) a
+      else go(n-1, b, a + b)
+
+    go(n, 0, 1)
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(formatAbs(-42))
+    println(formatFactorial(7))
+  }
 
 }
