23fcf01fbf963773350aa658fe28be5e971d17e6
Chris Howe-Jones <chris.howejones@gmail.com>
2017-10-16 16:41:23 +0100

    Change to curry dropWhile for better type inference

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -2,18 +2,18 @@ package fpinscala.datastructures
 
 sealed trait List[+A]
 case object Nil extends List[Nothing]
-case class Cons[+A] (head: A, tail: List[A]) extends List[A]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
   def sum(ints: List[Int]): Int = ints match {
     case Nil => 0
-    case Cons(x,xs) => x + sum(xs)
+    case Cons(x, xs) => x + sum(xs)
   }
 
   def product(ds: List[Double]): Double = ds match {
     case Nil => 1.0
     case Cons(0.0, _) => 0.0
-    case Cons(x,xs) => x * product(xs)
+    case Cons(x, xs) => x * product(xs)
   }
 
   def apply[A](as: A*): List[A] =
@@ -30,28 +30,28 @@ object List {
     case Cons(_, t) => Cons(n, t)
   }
 
-  def drop[A] (l: List[A], n: Int): List[A] =
+  def drop[A](l: List[A], n: Int): List[A] =
     if (n <= 0) l
     else l match {
       case Nil => Nil
-      case Cons(_, t) => drop(t, n-1)
+      case Cons(_, t) => drop(t, n - 1)
     }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+  def dropWhile[A](l: List[A])(f: A => Boolean): List[A] = l match {
     case Nil => Nil
-    case Cons(h, t) => if (f(h)) dropWhile(t, f) else l
+    case Cons(h, t) => if (f(h)) dropWhile(t)(f) else l
   }
 
   def append[A](a1: List[A], a2: List[A]): List[A] =
     a1 match {
       case Nil => a2
-      case Cons(h,t) => Cons(h, append(t, a2))
+      case Cons(h, t) => Cons(h, append(t, a2))
     }
 
   def init[A](l: List[A]): List[A] = l match {
     case Nil => Nil
-    case Cons(h,Cons(h2, Nil)) => Cons(h, Nil)
-    case Cons(h,t) => Cons(h, init(t))
+    case Cons(h, Cons(h2, Nil)) => Cons(h, Nil)
+    case Cons(h, t) => Cons(h, init(t))
   }
 
 }
