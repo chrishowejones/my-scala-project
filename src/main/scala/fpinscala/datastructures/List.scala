c451e3ea10ab846abd5f8382522ac69ca2ba5cd1
Chris Howe-Jones <chris.howejones@gmail.com>
2017-10-16 16:18:57 +0100

    Implemented dropWhile on List

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -37,4 +37,9 @@ object List {
       case Cons(_, t) => drop(t, n-1)
     }
 
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(h, t) => if (f(h)) dropWhile(t, f) else l
+  }
+
 }
