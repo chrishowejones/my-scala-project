50705d25cd64572b8c8b1dd2d6702971f1c59bf0
Chris Howe-Jones <chris.howejones@gmail.com>
2017-10-16 15:37:13 +0100

    Currying and composition

--- src/main/scala/example/MyModule.scala
+++ src/main/scala/example/MyModule.scala
@@ -54,6 +54,12 @@ object MyModule {
     loop(0)
   }
 
+  def curry[A,B,C](f: (A,B) => C): A => (B => C) = a => (b => (f(a,b)))
+
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C = (a, b) => (f(a)(b))
+
+  def compose[A,B,C](f: B => C, g: A => B): A => C = a => f(g(a))
+
   def main(args: Array[String]): Unit = {
     println(formatResult("abs value", -42, abs))
     println(formatResult("factorial", 7, factorial))
