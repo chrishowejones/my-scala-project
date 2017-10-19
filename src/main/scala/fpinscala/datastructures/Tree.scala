87bbde0e137b83a401b116754e9812b5ea88eba7
Chris Howe-Jones <chris.howejones@gmail.com>
2017-10-19 08:43:02 +0100

    Implemented map for Tree

--- src/main/scala/fpinscala/datastructures/Tree.scala
+++ src/main/scala/fpinscala/datastructures/Tree.scala
@@ -47,4 +47,11 @@ object Tree {
     maxPath(t, 0)
   }
 
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = {
+    t match {
+      case Leaf(a) => Leaf(f(a))
+      case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+    }
+  }
+
 }
