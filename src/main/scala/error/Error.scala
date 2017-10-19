bef3dc74b273b54a5ba246439a0c568233396102
Chris Howe-Jones <chris.howejones@gmail.com>
2017-10-19 14:46:53 +0100

    Implement flatMap for Option

--- src/main/scala/error/Error.scala
+++ src/main/scala/error/Error.scala
@@ -1,15 +1,21 @@
 package error
 
-sealed trait Option[+A] {
+trait Option[+A] {
   def map[B](f: A => B): Option[B] =
     this match {
       case None => None
       case Some(a) => Some(f(a))
     }
-  /* def flatMap[B](f: A=> Option[B]): Option[B]
-   * def getOrElse[B >: A](default: => B):B
-   * def orElse[B >: A](ob: => Option[B]): Option[B]
-   * def filter(f: A => Boolean): Option[A] */
+
+  def flatMap_1[B](f: A => Option[B]): Option[B] = this match {
+    case None => None
+    case Some(a) => f(a)
+  }
+
+
+  def getOrElse[B >: A](default: => B):B = ???
+  def orElse[B >: A](ob: => Option[B]): Option[B] = ???
+  def filter(f: A => Boolean): Option[A] = ???
 }
 
 case class Some[+A](get: A) extends Option[A]
