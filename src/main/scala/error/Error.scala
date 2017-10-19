b7ff0488154a98e0d1d777e3ef39d3203014cc14
Chris Howe-Jones <chris.howejones@gmail.com>
2017-10-19 14:32:10 +0100

    Implement map on Option

--- src/main/scala/error/Error.scala
+++ src/main/scala/error/Error.scala
@@ -1,6 +1,17 @@
 package error
 
-sealed trait Option[+A]
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] =
+    this match {
+      case None => None
+      case Some(a) => Some(f(a))
+    }
+  /* def flatMap[B](f: A=> Option[B]): Option[B]
+   * def getOrElse[B >: A](default: => B):B
+   * def orElse[B >: A](ob: => Option[B]): Option[B]
+   * def filter(f: A => Boolean): Option[A] */
+}
+
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
