12ec9039790014c2066c2d5a052213e43509f8e5
Chris Howe-Jones <chris.howejones@gmail.com>
2017-11-05 16:40:24 +0000

    Implement flatMap using getOrElse

--- src/main/scala/error/Error.scala
+++ src/main/scala/error/Error.scala
@@ -7,24 +7,23 @@ trait Option[+A] {
       case Some(a) => Some(f(a))
     }
 
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None => default
+    case Some(a) => a
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    map(f) getOrElse None
+
   def flatMap_1[B](f: A => Option[B]): Option[B] = this match {
     case None => None
     case Some(a) => f(a)
   }
 
 
-  def getOrElse[B >: A](default: => B):B = ???
   def orElse[B >: A](ob: => Option[B]): Option[B] = ???
   def filter(f: A => Boolean): Option[A] = ???
 }
 
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
-
-object Try {
-
-  def mean(xs: Seq[Double]): Option[Double] =
-    if (xs.isEmpty) None
-    else Some(xs.sum / xs.length)
-
-}
