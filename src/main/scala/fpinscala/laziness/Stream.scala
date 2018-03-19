d00119058e68c7a7fc75a4ecae11ac0ba7a0579b
Chris Howe-Jones <chris.howejones@gmail.com>
2018-03-19 15:31:44 +0000

    Implement take with unfold.

--- src/main/scala/fpinscala/laziness/Stream.scala
+++ src/main/scala/fpinscala/laziness/Stream.scala
@@ -85,6 +85,12 @@ trait Stream[+A] {
       case _ => None
     }
 
+  def take2(n: Int): Stream[A] =
+    unfold((this, n)){
+      case (Cons(h,t), 1) => Some((h(), (empty,0)))
+      case (Cons(h,t), n) if (n > 1) => Some((h(), (t(), n - 1)))
+      case _ => None
+    }
 }
 
 case object Empty extends Stream[Nothing]
