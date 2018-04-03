47e72654d3e3438e4f9fd6172e1eee0c266fcd4e
Chris Howe-Jones <chris.howejones@gmail.com>
2018-04-03 15:50:14 +0100

    Implement sequence and ints with sequence.

--- src/main/scala/fpinscala/state/State.scala
+++ src/main/scala/fpinscala/state/State.scala
@@ -13,7 +13,7 @@ object RNG {
       val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL // `&` is bitwise AND. We use the current seed to generate a new seed.
       val nextRNG = Simple(newSeed) // The next state, which is an `RNG` instance created from the new seed.
       val n = (newSeed >>> 16).toInt // `>>>` is right binary shift with zero fill. The value `n` is our new pseudo-random integer.
-      (n, nextRNG) // The return value is a tuple containing both a pseudo-random integer and the next `RNG` state.
+        (n, nextRNG) // The return value is a tuple containing both a pseudo-random integer and the next `RNG` state.
     }
   }
 
@@ -65,13 +65,16 @@ object RNG {
   }
 
   def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
-    if (count <= 0)
-      (List(), rng)
-    else {
-      val (i, r)  = rng.nextInt
-      val (is, r2) = ints(count - 1)(r)
-      (i :: is, r2)
+    def go(c: Int, r: RNG, xs: List[Int]): (List[Int], RNG) = {
+      if (c <= 0)
+        (xs, r)
+      else {
+        val (x, r2)  = r.nextInt
+        go(c - 1, r2, x :: xs)
+      }
     }
+
+    go(count, rng, List())
   }
 
   def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
@@ -90,7 +93,11 @@ object RNG {
   def randDoubleInt: Rand[(Double, Int)] =
     both(double, int)
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
+
+  def intsWithSequence(count: Int): Rand[List[Int]] =
+    sequence(List.fill(count)(int))
 
   def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
 }
