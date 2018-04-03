fb03f53b80f5033578f80567414d1bbf5ad14285
Chris Howe-Jones <chris.howejones@gmail.com>
2018-04-03 11:00:39 +0100

    Implement ints.

--- src/main/scala/fpinscala/state/State.scala
+++ src/main/scala/fpinscala/state/State.scala
@@ -59,7 +59,15 @@ object RNG {
     ((d1, d2, d3), r3)
   }
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    if (count <= 0)
+      (List(), rng)
+    else {
+      val (i, r)  = rng.nextInt
+      val (is, r2) = ints(count - 1)(r)
+      (i :: is, r2)
+    }
+  }
 
   def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
 
